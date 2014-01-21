66e303cba6d93bf10feeac69db202c1a3f7abd79
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2014-01-21 20:11:11 -0300

    first commit of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Workshop4presencial::Application.config.secret_key_base = '02b464f13d4d09afab402aa512d3ca7c02f8a69be2d921eef212c6ffa185c3336c072407b2d98bb142d515e74fe0380258b35156a76005e2de5bbf60b0a5136b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Workshop4presencial::Application.config.session_store :cookie_store, key: '_workshop4presencial_session'
